b3d17fdb43a5af2f703eb3203a78c34de3ef95fe
anandajuliar <anandajulia001@gmail.com>
2024-05-30 17:42:19 +0700

    Jika REAR berada di posisi terakhir array, kembali ke awal array

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -26,4 +26,9 @@ public:
         FRONT = 0;
         REAR = 0;
     }
-    
+    else {
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
